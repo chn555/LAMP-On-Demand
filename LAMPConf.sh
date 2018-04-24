00a73455a44aa446efea4fe8ea728fd47ef383d4
BigRush <BigRush@Rig.com>
2018-04-24 19:42:35 +0300

    config test

--- LAMPConf.sh
+++ LAMPConf.sh
@@ -18,9 +18,23 @@ Root_Check () {		## checks that the script runs as root
 	fi
 }
 
+Log_Path () {		## set log path and variables for installation logs, makes sure whether log folder exists and if not, create it
+	dialog_stderr_log=/var/log/LAMP-On-Demand/Error_dialog_install.log
+	dialog_stdout_log=/var/log/LAMP-On-Demand/dialog_install.log
+	web_install_stderr_log=/var/log/LAMP-On-Demand/Error_websrv_install.log
+	web_install_stdout_log=/var/log/LAMP-On-Demand/websrv_install.log
+	web_service_stderr_log=/var/log/LAMP-On-Demand/Error_websrv_service.log
+	web_service_stdout_log=/var/log/LAMP-On-Demand/websrv_service.log
+	log_folder=/var/log/LAMP-On-Demand
+
+	if [[ -d $log_folder ]]; then
+		:
+	else
+		mkdir $log_folder
+	fi
+}
 
 Distro_Check () {		## checking the environment the user is currenttly running on to determine which settings should be applied
-
 	cat /etc/*-release |grep ID |cut  -d "=" -f "2" |egrep "^arch$|^manjaro$" &> /dev/null
 
 	if [[ $? -eq 0 ]]; then
@@ -47,11 +61,6 @@ Distro_Check () {		## checking the environment the user is currenttly running on
 }
 
 Dialog_Check () {		## checks if dialog is installed, if it doesn't then install dialog
-
-	####Variables & Function calls####
-	dialog_stderr_log=/var/log/LAMPConf/Error_dialog_install.log
-	dialog_stdout_log=/var/log/LAMPConf/dialog_install.log
-	####Variables & Function calls####
 	command -v dialog
 	if [[ $? -eq 0 ]]; then
 		:
@@ -74,7 +83,7 @@ Dialog_Check () {		## checks if dialog is installed, if it doesn't then install 
 				else
 					printf "$line\n"
 					printf "Something went wrong during dialog installation\n"
-					printf "Please check the log file under /log/LAMPConf/Error_dialog_install.log\n"
+					printf "Please check the log file under /var/log/LAMP-On-Demand/Error_dialog_install.log\n"
 					printf "$line\n"
 				fi
 		elif [[ $answer =~ [n|N] ]]; then
@@ -86,16 +95,15 @@ Dialog_Check () {		## checks if dialog is installed, if it doesn't then install 
 	fi
 
 }
-Web_server_Installation () {		## choose which web server would you like to install
 
+Web_server_Installation () {		## choose which web server would you like to install
 	####Variables & Function calls####
 	For_The_Looks
 	Root_Check
 	Distro_Check
+	Log_Path
 	Dialog_Check
-	web_stderr_log=/var/log/LAMPConf/Error_websrv_install.log
-	web_stdout_log=/var/log/LAMPConf/websrv_install.log
-	local PS3="Please select the web server that you would like to install and press enters: "
+	# local PS3="Please select the web server that you would like to install and press enters: "
 	####Variables & Function calls####
 
 	## prompt the user with a menu to select whether to install apache or nginx web server
@@ -108,9 +116,9 @@ Web_server_Installation () {		## choose which web server would you like to insta
 	case $web_server in
 		Apache)
 			if [[ $Distro_Val =~ "centos" ]]; then
-				yum install httpd -y 2> $web_stderr_log > $web_stdout_log
+				yum install httpd -y 2>> $web_install_stderr_log >> $web_install_stdout_log
 			elif [[ $Distro_Val =~ "debian" ]]; then
-				apt-get install apache2 -y 2> $web_stderr_log > $web_stdout_log
+				apt-get install apache2 -y 2>> $web_install_stderr_log >> $web_install_stdout_log
 			fi
 			if [[ $? -eq 0 ]]; then
 				printf "$line\n"
@@ -120,15 +128,15 @@ Web_server_Installation () {		## choose which web server would you like to insta
 			else
 				printf "$line\n"
 				printf "Something went wrong during Apache installation\n"
-				printf "Please check the log file under /log/LAMPConf/Error_websrv_install.log"
+				printf "Please check the log file under $web_install_stderr_log\n"
 				printf "$line\n"
 			fi
 			;;
 		Ngnix)
 			if [[ $Distro_Val =~ "centos" ]]; then
-				yum --enablerepo=epel -y install nginx 2> web_stderr_log > web_stdout_log
+				yum --enablerepo=epel -y install nginx 2>> $web_install_stderr_log >> $web_install_stdout_log
 			elif [[ $Distro_Val =~ "debian" ]]; then
-				apt-get install nginx -y
+				apt-get install nginx -y 2>> $web_install_stderr_log >> $web_install_stdout_log
 			fi
 			if [[ $? -eq 0 ]]; then
 				printf "$line\n"
@@ -138,7 +146,7 @@ Web_server_Installation () {		## choose which web server would you like to insta
 			else
 				printf "$line\n"
 				printf "Something went wrong during Ngnix installation\n"
-				printf "Please check the log file under /log/LAMPConf/Error_websrv_install.log"
+				printf "Please check the log file under $web_install_stderr_log\n"
 				printf "$line\n"
 			fi
 			;;
@@ -152,31 +160,70 @@ Web_server_Installation () {		## choose which web server would you like to insta
 
 	}
 
-Web_Server_Configuration () {
-
+Web_Server_Configuration () {		## start the web server's service
 	Web_server_Installation
 
 	if [[ $web_server =~ "Apache" ]]; then
 		if [[ $Distro_Val =~ "centos" ]]; then
-			systemctl restart httpd
+			systemctl enable httpd 2>> $web_service_stderr_log >> $web_service_stdout_log
+			if [[ $? -eq 0 ]]; then
+				:
+			else
+				printf "$line\n"
+				printf "Something went wrong while enabling the service\n"
+				printf "Please check the log file under $web_service_stderr_log\n"
+				printf "$line\n"
+				exit
+			fi
+			systemctl restart httpd 2>> $web_service_stderr_log >> $web_service_stdout_log
+			httpd_exit=$?
 		elif [[ $Distro_Val =~ "debian" ]]; then
-			systemctl restart apache2
+			systemctl enable apache2 2>> $web_service_stderr_log >> $web_service_stdout_log
+			if [[ $? -eq 0 ]]; then
+				:
+			else
+				printf "$line\n"
+				printf "Something went wrong while enabling the service\n"
+				printf "Please check the log file under $web_service_stderr_log\n"
+				printf "$line\n"
+				exit
+			fi
+			systemctl restart apache2 2>> $web_service_stderr_log >> $web_service_stdout_log
+			apache_exit=$?
 		fi
-		if [[ $? -eq 0 ]] ;then
+		if [[ $httpd_exit == 0 || $apache_exit == 0  ]] ;then
 			printf "$line\n"
 			printf "Apache web server is up and running!"
 			printf "$line\n"
 		else
-			printf "Something went wrong... sorry :(\n"
+			printf "$line\n"
+			printf "Something went wrong while enabling the service\n"
+			printf "Please check the log file under $web_service_stderr_log\n"
+			printf "$line\n"
+			exit
 		fi
 	elif [[ $web_server =~ "Nginx" ]]; then
+		systemctl enable nginx 2>> $web_service_stderr_log >> $web_service_stdout_log
+		if [[ $? -eq 0 ]] ;then
+			:
+		else
+			printf "$line\n"
+			printf "Something went wrong while enabling the service\n"
+			printf "Please check the log file under $web_service_stderr_log\n"
+			printf "$line\n"
+			exit
+		fi
 		systemctl restart nginx
 		if [[ $? -eq 0 ]] ;then
 			printf "$line\n"
-			printf "Apache web server is up and running!"
+			printf "Nginx web server is up and running!"
 			printf "$line\n"
 		else
-			printf "Something went wrong... sorry :(\n"
+			printf "$line\n"
+			printf "Something went wrong while enabling the service\n"
+			printf "Please check the log file under $web_service_stderr_log\n"
+			printf "$line\n"
+			exit
 		fi
 	fi
 }
