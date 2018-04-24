a66b5346f0b13152617ead7ba881ba9b02b01654
tom <tom@arch>
2018-04-24 12:10:10 +0300

    first test

--- LAMPConf.sh
+++ LAMPConf.sh
@@ -45,30 +45,47 @@ Distro_Check () {		## checking the environment the user is currenttly running on
 }
 
 
-Web_server_Installation () {
+Web_server_Installation () {		## choose which web server would you like to install
 
+####Variables####
+	For_The_Looks
 	web_stderr_log=/log/LAMPConf/Error_websrv_install.log
 	web_stdout_log=/log/LAMPConf/websrv_install.log
 	web_srv=(Apache Ngnix)
+	local PS3="Please select the web server that you would like to install: "
+####Variables####
 
+	## prompt the user with a menu to select whether to install apache or nginx web server
 	select opt in ${web_srv[@]} ;do
 		case opt in
 			Apache)
 				yum install httpd -y 2> web_stderr_log > web_stdout_log
 				if [[ $? -eq 0 ]] ;then
+					printf "$line\n"
 					printf "Apache installation completed successfully\n"
+					printf "$line\n"
 				else
+					printf "$line\n"
 					printf "Something went wrong during Apache installation\n"
 					printf "Please check the log file under /log/LAMPConf/Error_websrv_install.log"
+					printf "$line\n"
 				fi
 				;;
 			Ngnix)
 				yum --enablerepo=epel -y install nginx
 				if [[ $? -eq 0 ]] ;then
+					printf "$line\n"
 					printf "Ngnix installation completed successfully\n"
+					printf "$line\n"
 				else
+					printf "$line\n"
 					printf "Something went wrong during Ngnix installation\n"
 					printf "Please check the log file under /log/LAMPConf/Error_websrv_install.log"
+					printf "$line\n"
 				fi
 				;;
+			esac
 }
+Distro_Check
+Root_Check
+Web_server_Installation
