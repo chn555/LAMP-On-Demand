ad5d7b424cb1d8a5226da0554354c8d015283a49
BigRush <BigRush@Rig.com>
2018-04-27 10:57:28 +0300

    major changes, probably distroyed everything, learning on the go, sorry future me...

--- LAMPConf.sh
+++ LAMPConf.sh
@@ -18,18 +18,23 @@ Log_And_Variables () {		## set log path and variables for installation logs, mak
 	####Variables####
 	line=#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!
 	whiptail_install_stderr_log=/var/log/LAMP-On-Demand/Error_whiptail_install.log
-	web_install_stderr_log=/var/log/LAMP-On-Demand/Error_websrv_install.log
-	sql_install_stderr_log=/var/log/LAMP-On-Demand/Error_sqlsrv_install.log
-	lang_install_stderr_log=/var/log/LAMP-On-Demand/Error_lang_install.log
-	web_service_stderr_log=/var/log/LAMP-On-Demand/Error_websrv_service.log
-	sql_service_stderr_log=/var/log/LAMP-On-Demand/Error_sqlsrv_service.log
 	whiptail_install_stdout_log=/var/log/LAMP-On-Demand/whiptail_install.log
+	web_install_stderr_log=/var/log/LAMP-On-Demand/Error_websrv_install.log
 	web_install_stdout_log=/var/log/LAMP-On-Demand/websrv_install.log
-	sql_install_stdout_log=/var/log/LAMP-On-Demand/sqlsrv_install.log
-	lang_install_stdout_log=/var/log/LAMP-On-Demand/lang_install.log
+	web_service_stderr_log=/var/log/LAMP-On-Demand/Error_websrv_service.log
 	web_service_stdout_log=/var/log/LAMP-On-Demand/websrv_service.log
+	sql_install_stderr_log=/var/log/LAMP-On-Demand/Error_sqlsrv_install.log
+	sql_install_stdout_log=/var/log/LAMP-On-Demand/sqlsrv_install.log
 	sql_service_stdout_log=/var/log/LAMP-On-Demand/sqlsrv_service.log
+	sql_service_stderr_log=/var/log/LAMP-On-Demand/Error_sqlsrv_service.log
+	lang_install_stderr_log=/var/log/LAMP-On-Demand/Error_lang_install.log
+	lang_install_stdout_log=/var/log/LAMP-On-Demand/lang_install.log
+	lang_service_stderr_log=/var/log/LAMP-On-Demand/Error_lang_service.log
+	lang_service_stdout_log=/var/log/LAMP-On-Demand/lang_service.log
+	remi_reop_stderr_log=/var/log/LAMP-On-Demand/Error_remi_repo.log
+	remi_reop_stdout_log=/var/log/LAMP-On-Demand/remi_repo.log
 	firewall_log=/var/log/LAMP-On-Demand/firewall.log
+	php_conf_file=/etc/httpd/conf.d/php.conf
 	log_folder=/var/log/LAMP-On-Demand
 	tempLAMP=$log_folder/LAMP_choise.tmp
 	apache_index_path=/var/www/html
@@ -112,9 +117,55 @@ Whiptail_Check () {		## checks if whiptail is installed, if it doesn't then inst
 		done
 		if [[ $answer =~ [y|Y] ]]; then
 			if [[ $Distro_Val =~ "centos" ]]; then
-				yum install whiptail -y 2>> $whiptail_install_stderr_log_log >> $whiptail_install_stdout_log_log
+				yum install whiptail -y 2>> $whiptail_install_stderr_log_log >> $whiptail_install_stdout_log_log &
+				{
+					i=3
+					while true ;do
+						ps aux |egrep -Eo "$!" &> /dev/null
+						if [[ $? -eq 0 ]]; then
+							if [[ $i -le 94 ]]; then
+								printf "$i\n"
+								i=$(expr $i + 7)
+								sleep 2.5
+							else
+								:
+							fi
+						else
+							break
+						fi
+					done
+					printf "96\n"
+					sleep 0.5
+					printf "98\n"
+					sleep 0.5
+					printf "100\n"
+					sleep 1
+				} |whiptail --gauge "Please wait while installing..." 6 50 0
 			elif [[ $Distro_Val =~ "debian" ]]; then
-				apt-get install whiptail -y 2>> $whiptail_install_stderr_log_log >> $whiptail_install_stdout_log_log
+				apt-get install whiptail -y 2>> $whiptail_install_stderr_log_log >> $whiptail_install_stdout_log_log &
+				{
+					i=3
+					while true ;do
+						ps aux |egrep -Eo "$!" &> /dev/null
+						if [[ $? -eq 0 ]]; then
+							if [[ $i -le 94 ]]; then
+								printf "$i\n"
+								i=$(expr $i + 7)
+								sleep 2.5
+							else
+								:
+							fi
+						else
+							break
+						fi
+					done
+					printf "96\n"
+					sleep 0.5
+					printf "98\n"
+					sleep 0.5
+					printf "100\n"
+					sleep 1
+				} |whiptail --gauge "Please wait while installing..." 6 50 0
 			fi
 				if [[ $? -eq 0 ]]; then
 					:
@@ -145,9 +196,55 @@ Web_Server_Installation () {		## choose which web server would you like to insta
 
 	if [[ $(cat $tempLAMP) =~ "Apache" ]]; then
 		if [[ $Distro_Val =~ "centos" ]]; then
-			yum install httpd -y 2>> $web_install_stderr_log >> $web_install_stdout_log
+			yum install httpd -y 2>> $web_install_stderr_log >> $web_install_stdout_log &
+			{
+				i=3
+				while true ;do
+					ps aux |egrep -Eo "$!" &> /dev/null
+					if [[ $? -eq 0 ]]; then
+						if [[ $i -le 94 ]]; then
+							printf "$i\n"
+							i=$(expr $i + 7)
+							sleep 2.5
+						else
+							:
+						fi
+					else
+						break
+					fi
+				done
+				printf "96\n"
+				sleep 0.5
+				printf "98\n"
+				sleep 0.5
+				printf "100\n"
+				sleep 1
+			} |whiptail --gauge "Please wait while installing..." 6 50 0
 		elif [[ $Distro_Val =~ "debian" ]]; then
-			apt-get install apache2 -y 2>> $web_install_stderr_log >> $web_install_stdout_log
+			apt-get install apache2 -y 2>> $web_install_stderr_log >> $web_install_stdout_log &
+			{
+				i=3
+				while true ;do
+					ps aux |egrep -Eo "$!" &> /dev/null
+					if [[ $? -eq 0 ]]; then
+						if [[ $i -le 94 ]]; then
+							printf "$i\n"
+							i=$(expr $i + 7)
+							sleep 2.5
+						else
+							:
+						fi
+					else
+						break
+					fi
+				done
+				printf "96\n"
+				sleep 0.5
+				printf "98\n"
+				sleep 0.5
+				printf "100\n"
+				sleep 1
+			} |whiptail --gauge "Please wait while installing..." 6 50 0
 		fi
 		if [[ $? -eq 0 ]]; then
 			printf "$line\n"
@@ -317,7 +414,30 @@ Sql_Server_Installation () {		## choose which data base server would you like to
 
 	if [[ "$(cat $tempLAMP)" =~ "MariaDB" ]]; then
 		if [[ $Distro_Val =~ "centos" ]]; then
-			yum install mariadb-server mariadb -y 2>> $sql_install_stderr_log >> $sql_install_stdout_log
+			yum install mariadb-server mariadb -y 2>> $sql_install_stderr_log >> $sql_install_stdout_log &
+			{
+				i=3
+				while true ;do
+					ps aux |egrep -Eo "$!" &> /dev/null
+					if [[ $? -eq 0 ]]; then
+						if [[ $i -le 94 ]]; then
+							printf "$i\n"
+							i=$(expr $i + 7)
+							sleep 2.5
+						else
+							:
+						fi
+					else
+						break
+					fi
+				done
+				printf "96\n"
+				sleep 0.5
+				printf "98\n"
+				sleep 0.5
+				printf "100\n"
+				sleep 1
+			} |whiptail --gauge "Please wait while installing..." 6 50 0
 		elif [[ $Distro_Val =~ "debian" ]]; then
 			apt-get install mariadb-server mariadb-client -y 2>> $sql_install_stderr_log >> $sql_install_stdout_log
 		fi
@@ -336,9 +456,55 @@ Sql_Server_Installation () {		## choose which data base server would you like to
 
 	elif [[ "$(cat $tempLAMP)" =~ "PostgreSQL" ]]; then
 		if [[ $Distro_Val =~ "centos" ]]; then
-			yum install postgresql-server postgresql-contrib -y 2>> $sql_install_stderr_log >> $sql_install_stdout_log
+			yum install postgresql-server postgresql-contrib -y 2>> $sql_install_stderr_log >> $sql_install_stdout_log &
+			{
+				i=3
+				while true ;do
+					ps aux |egrep -Eo "$!" &> /dev/null
+					if [[ $? -eq 0 ]]; then
+						if [[ $i -le 94 ]]; then
+							printf "$i\n"
+							i=$(expr $i + 7)
+							sleep 2.5
+						else
+							:
+						fi
+					else
+						break
+					fi
+				done
+				printf "96\n"
+				sleep 0.5
+				printf "98\n"
+				sleep 0.5
+				printf "100\n"
+				sleep 1
+			} |whiptail --gauge "Please wait while installing..." 6 50 0
 		elif [[ $Distro_Val =~ "debian" ]]; then
-			apt-get install postgresql postgresql-contrib -y 2>> $sql_install_stderr_log >> $sql_install_stdout_log
+			apt-get install postgresql postgresql-contrib -y 2>> $sql_install_stderr_log >> $sql_install_stdout_log &
+			{
+				i=3
+				while true ;do
+					ps aux |egrep -Eo "$!" &> /dev/null
+					if [[ $? -eq 0 ]]; then
+						if [[ $i -le 94 ]]; then
+							printf "$i\n"
+							i=$(expr $i + 7)
+							sleep 2.5
+						else
+							:
+						fi
+					else
+						break
+					fi
+				done
+				printf "96\n"
+				sleep 0.5
+				printf "98\n"
+				sleep 0.5
+				printf "100\n"
+				sleep 1
+			} |whiptail --gauge "Please wait while installing..." 6 50 0
 		fi
 		if [[ $? -eq 0 ]]; then
 			printf "$line\n"
@@ -471,22 +637,165 @@ Sql_Server_Configuration () {		## configure data base
 Lang_Installation () {	## installs language support of user choice
 	whiptail --title "LAMP-On-Demand" \
 	--menu "Please choose lang server to install:" 15 55 5 \
-	"PHP 5.4" "PHP Version 5.4" \
+	"PHP 5.4" "PHP Version 5.4 (***CentOS_7 only***)" \
 	"PHP 7.0" "PHP Version 7.0" \
-	"Python" "Python Version 2.7" \
+	"Python" "Python Version 3" \
 	"Exit" "Walk away from the path to LAMP stack :(" 2> $tempLAMP
 
-	if [[ "$(cat $tempLAMP)" =~ "PHP" ]]; then
+	if [[ "$(cat $tempLAMP)" == "PHP 5.4" ]]; then
 		if [[ $Distro_Val =~ "centos" ]]; then
 			yum install php php-mysql -y 2>> $lang_install_stderr_log >> $lang_install_stdout_log
 		elif [[ $Distro_Val =~ "debian" ]]; then
-			apt-get install php php-mysql -y 2>> $lang_install_stderr_log >> $sql_install_stdout_log
+			whiptail --title "LAMP-On-Demand" \
+			--msgbox "\nSorry, this script doesn't support php 5.4 installation for Debian." 8 78
+			Lang_Installation
 		fi
 		systemctl restart httpd 2>> $web_service_stderr_log >> $web_service_stdout_log
 		if [[ $? -eq 0 ]]; then
-			printf "$line\n"
-			printf "PHP support is up and running!"
-			printf "$line\n"
+			whiptail --title "LAMP-On-Demand" \
+			--msgbox "\nPHP support is up and running!" 8 78
+			# Main_Menu
+		else
+			whiptail --title "LAMP-On-Demand" \
+			--msgbox "\nSomething went wrong while enabling the service.\nPlease check the log file under $web_service_stderr_log" 8 78
+			exit 1
+		fi
+	elif [[ "$(cat $tempLAMP)" == "PHP 7.0" ]]; then
+		if [[ $Distro_Val =~ "centos" ]]; then
+			yum -y install http://rpms.famillecollet.com/enterprise/remi-release-7.rpm 2>> $remi_reop_stderr_log >> $remi_reop_stdout_log &
+			{
+				i=3
+				while true ;do
+					ps aux |egrep -Eo "$!" &> /dev/null
+					if [[ $? -eq 0 ]]; then
+						if [[ $i -le 94 ]]; then
+							printf "$i\n"
+							i=$(expr $i + 7)
+							sleep 2.5
+						else
+							:
+						fi
+					else
+						break
+					fi
+				done
+				printf "96\n"
+				sleep 0.5
+				printf "98\n"
+				sleep 0.5
+				printf "100\n"
+				sleep 1
+			} |whiptail --gauge "Please wait while installing..." 6 50 0
+			if [[ $? -eq 0 ]]; then
+				whiptail --title "LAMP-On-Demand" \
+				--msgbox "\nRemi's reop installation complete." 8 78
+			else
+				whiptail --title "LAMP-On-Demand" \
+				--msgbox "\nSomething went wrong, Remi's reop installation failed." 8 78
+				exit 1
+			fi
+			yum --enablerepo=remi-safe -y install php70 php70-php-pear php70-php-mbstring php70-php-fpm 2>> $lang_install_stderr_log >> $sql_install_stdout_log &
+			{
+				i=3
+				while true ;do
+					ps aux |egrep -Eo "$!" &> /dev/null
+					if [[ $? -eq 0 ]]; then
+						if [[ $i -le 94 ]]; then
+							printf "$i\n"
+							i=$(expr $i + 7)
+							sleep 2.5
+						else
+							:
+						fi
+					else
+						break
+					fi
+				done
+				printf "96\n"
+				sleep 0.5
+				printf "98\n"
+				sleep 0.5
+				printf "100\n"
+				sleep 1
+			} |whiptail --gauge "Please wait while installing..." 6 50 0
+			if [[ $? -eq 0 ]]; then
+				whiptail --title "LAMP-On-Demand" \
+				--msgbox "\nPHP 7.0 installation complete." 8 78
+			else
+				whiptail --title "LAMP-On-Demand" \
+				--msgbox "\nSomething went wrong, PHP 7.0 installation failed." 8 78
+				exit 1
+			fi
+			sed -ie 's/SetHandler.*/SetHandler \"proxy:fcgi://127.0.0.1:9000\"/' $php_conf_file
+			if [[ $? -eq 0 ]]; then
+				:
+				systemctl restart httpd 2>> $web_service_stderr_log >> $web_service_stdout_log
+				if [[ $? -eq 0 ]]; then
+					whiptail --title "LAMP-On-Demand" \
+					--msgbox "\nPHP 7.0 support is up and running." 8 78
+				else
+					printf "$line\n"
+					printf "Something went wrong while enabling the service\n"
+					printf "Please check the log file under $web_service_stderr_log\n"
+					printf "$line\n"
+					exit 1
+				fi
+			else
+				whiptail --title "LAMP-On-Demand" \
+				--msgbox "\nThere was a problem with sed command or the \"php.conf\" file doesn't exists" 8 78
+				exit 1
+			fi
+
+		elif [[ $Distro_Val =~ "debian" ]]; then
+			apt-get install php7.0 php7.0-mysql libapache2-mod-php7.0 -y 2>> $lang_install_stderr_log >> $sql_install_stdout_log &
+			{
+				i=3
+				while true ;do
+					ps aux |egrep -Eo "$!" &> /dev/null
+					if [[ $? -eq 0 ]]; then
+						if [[ $i -le 94 ]]; then
+							printf "$i\n"
+							i=$(expr $i + 7)
+							sleep 2.5
+						else
+							:
+						fi
+					else
+						break
+					fi
+				done
+				printf "96\n"
+				sleep 0.5
+				printf "98\n"
+				sleep 0.5
+				printf "100\n"
+				sleep 1
+			} |whiptail --gauge "Please wait while installing..." 6 50 0
+			if [[ $? -eq 0 ]]; then
+				whiptail --title "LAMP-On-Demand" \
+				--msgbox "\nPHP 7.0 installation complete." 8 78
+			else
+				whiptail --title "LAMP-On-Demand" \
+				--msgbox "\nSomething went wrong, Remi's reop installation failed." 8 78
+			fi
+
+	elif [[ "$(cat $tempLAMP)" =~ "Exit" ]]; then
+		printf "$line\n"
+		printf "Exit - I hope you feel safe now\n"
+		printf "$line\n"
+	fi
+}
+
+Lang_Configuration () {
+	if [[ $(cat $tempLAMP)]]
+
+	sed -ie 's/SetHandler.*/SetHandler \"proxy:fcgi://127.0.0.1:9000\"/' $php_conf_file
+	if [[ $? -eq 0 ]]; then
+		:
+		systemctl restart httpd 2>> $web_service_stderr_log >> $web_service_stdout_log
+		if [[ $? -eq 0 ]]; then
+			whiptail --title "LAMP-On-Demand" \
+			--msgbox "\nPHP 7.0 support is up and running." 8 78
 		else
 			printf "$line\n"
 			printf "Something went wrong while enabling the service\n"
@@ -494,12 +803,14 @@ Lang_Installation () {	## installs language support of user choice
 			printf "$line\n"
 			exit 1
 		fi
-	elif [[ "$(cat $tempLAMP)" =~ "Exit" ]]; then
-		printf "$line\n"
-		printf "Exit - I hope you feel safe now\n"
-		printf "$line\n"
+	else
+		whiptail --title "LAMP-On-Demand" \
+		--msgbox "\nThere was a problem with sed command or the \"php.conf\" file doesn't exists" 8 78
+		exit 1
 	fi
 }
+
+
 Root_Check
 Distro_Check
 Log_And_Variables
