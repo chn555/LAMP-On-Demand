9b94566dedfeb367c3f97ab1e0a2de46df38e6cd
chn555 <chn566@gmail.com>
2018-04-29 21:54:03 +0300

    s

--- LAMPConf.sh
+++ LAMPConf.sh
@@ -265,8 +265,7 @@ Web_Server_Installation () {		## choose which web server would you like to insta
 
 Web_Server_Configuration () {		## start the web server's service
 	if [[ "$(cat $tempLAMP)" =~ "Apache" ]]; then
-		cat $my_index_html > $apache_index_path << _EOF_
-		<html>
+		cat "<html>
 				<head>
 					<title>LAMP-On-Demand</title>
 				</head>
@@ -284,8 +283,7 @@ Web_Server_Configuration () {		## start the web server's service
 						<li>Debian</li>
 					</ul>
 					</body>
-			</html>
-_EOF_	
+			</html>  " > $apache_index_path
 
 		if [[ $Distro_Val =~ "centos" ]]; then
 			systemctl enable httpd 2>> $web_service_stderr_log >> $web_service_stdout_log
