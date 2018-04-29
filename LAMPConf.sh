cc40f515e8ec6ebd5b457e12836df52c89b82ba2
chn555 <chn566@gmail.com>
2018-04-29 21:36:39 +0300

    s

--- LAMPConf.sh
+++ LAMPConf.sh
@@ -42,7 +42,7 @@ Log_And_Variables () {		## set log path and variables for installation logs, mak
 	apache_index_path=/var/www/html/index.html
 	nginx_index_path=/usr/share/nginx/html
 	nginx_conf_path=/etc/conf.d/default.conf
-	my_index_html=$(printf "
+	my_index_html="
 	<!DOCTYPE html>
 	<html>
 		<head>
@@ -64,7 +64,7 @@ Log_And_Variables () {		## set log path and variables for installation logs, mak
 
 			</body>
 	</html>
-	")
+	"
 	nginx_conf_file=$(printf "
 	server {
     listen       80;
