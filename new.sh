3c796fc10a8fc322a983ac2bb27c5f6babbb8f51
chn555 <chn566@gmail.com>
2018-04-29 23:15:37 +0300

    s

--- new.sh
+++ new.sh
@@ -39,6 +39,42 @@ Distro_Check () {		## checking the environment the user is currenttly running on
 	fi
 }
 
+Log_And_Variables () {		## set log path and variables for installation logs, makes sure whether log folder exists and if not, create it
+	####Variables####
+	line=#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!
+	whiptail_install_stderr_log=/var/log/LAMP-On-Demand/Error_whiptail_install.log
+	whiptail_install_stdout_log=/var/log/LAMP-On-Demand/whiptail_install.log
+	web_install_stderr_log=/var/log/LAMP-On-Demand/Error_websrv_install.log
+	web_install_stdout_log=/var/log/LAMP-On-Demand/websrv_install.log
+	web_service_stderr_log=/var/log/LAMP-On-Demand/Error_websrv_service.log
+	web_service_stdout_log=/var/log/LAMP-On-Demand/websrv_service.log
+	sql_install_stderr_log=/var/log/LAMP-On-Demand/Error_sqlsrv_install.log
+	sql_install_stdout_log=/var/log/LAMP-On-Demand/sqlsrv_install.log
+	sql_service_stdout_log=/var/log/LAMP-On-Demand/sqlsrv_service.log
+	sql_service_stderr_log=/var/log/LAMP-On-Demand/Error_sqlsrv_service.log
+	lang_install_stderr_log=/var/log/LAMP-On-Demand/Error_lang_install.log
+	lang_install_stdout_log=/var/log/LAMP-On-Demand/lang_install.log
+	lang_service_stderr_log=/var/log/LAMP-On-Demand/Error_lang_service.log
+	lang_service_stdout_log=/var/log/LAMP-On-Demand/lang_service.log
+	remi_reop_stderr_log=/var/log/LAMP-On-Demand/Error_remi_repo.log
+	remi_reop_stdout_log=/var/log/LAMP-On-Demand/remi_repo.log
+	firewall_log=/var/log/LAMP-On-Demand/firewall.log
+	php_conf=/etc/httpd/conf.d/php.conf
+	php_fpm_conf=/etc/php-fpm.d/www.conf
+	php_ini_conf=/etc/php.ini
+	log_folder=/var/log/LAMP-On-Demand
+	tempLAMP=$log_folder/LAMP_choise.tmp
+	apache_index_path=/var/www/html/index.html
+	nginx_index_path=/usr/share/nginx/html
+	nginx_conf_path=/etc/conf.d/default.conf
+
+	if [[ -d $log_folder ]]; then
+		:
+	else
+		mkdir -p $log_folder
+	fi
+}
+
 Web_Server_Installation () {		## choose which web server would you like to install
 	## prompt the user with a menu to select whether to install apache or nginx web server
   Web_Server_Installation_Options="Apache Ngnix"
@@ -108,4 +144,5 @@ Main_Menu () {
 
 Root_Check
 Distro_Check
+Log_And_Variables
 Main_Menu
